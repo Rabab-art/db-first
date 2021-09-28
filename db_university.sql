266cf2dff2e62b808e7012eef4c81f97b42febbe
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-28 15:33:20 +0200

    SELECTED 2

--- db_university.sql
+++ db_university.sql
@@ -2,3 +2,10 @@
 SELECT `date_of_birth`
 FROM `students`
 WHERE YEAR(`date_of_birth`)= 1990;
+
+
+-- Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT `cfu`,`name`
+FROM `courses`
+WHERE (`cfu`) > 10;
+
