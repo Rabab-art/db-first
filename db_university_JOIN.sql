1f8a28eb391e4eedecbabe908016fe70b38b1cb8
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-29 22:54:41 +0200

    JOIN EX 3

--- db_university_JOIN.sql
+++ db_university_JOIN.sql
@@ -12,9 +12,14 @@ FROM `degrees`
 JOIN `departments` ON `degrees`. `department_id` = `departments`. `id` 
 WHERE `departments`.`name`= 'Dipartimento di Neuroscienze'
 
-
-
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.`name`
+FROM `courses`
+JOIN `course_teacher` ON `teachers`. `id` = `course_teacher`. `teacher_id` 
+JOIN `courses` ON `course_teacher`. `course_id` = `courses`. `id` 
+WHERE `teachers`.`id`= 44 OR  `teachers`.`name`= 'Fulvio Amato';
+
+
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
