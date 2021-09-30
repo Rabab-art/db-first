1a4013c7a5db5be0996b7b4bb03cef08c1934ada
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-30 21:31:44 +0200

    JOIN 5

--- db_university_JOIN.sql
+++ db_university_JOIN.sql
@@ -23,6 +23,22 @@ WHERE `teachers`.`surname`= 'Amato' AND `teachers`.`name`= 'Fulvio';
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a 
 cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT`students`. `name`, `students`. `surname`,`degrees`.`name`,`departments`.`name` 
+FROM `students` JOIN `degrees`ON`degrees` . `id` = `students`. `degree_id` 
+JOIN `departments` ON `departments`.`id`= `degrees` . `department_id` 
+ORDER BY `students` .`surname`, `students`.`name`;
+
+
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.*, `courses`.*,`teachers`.*
+FROM `degrees`
+JOIN`courses`
+ON `courses`. `degree_id`= `degrees`.`id`
+JOIN`course_teacher`
+ON `courses`.`id`= `course_teacher`.`course_id`
+JOIN`teachers`
+ON `teachers`.`id`= `course_teacher`.`teacher_id`
+ORDER BY `degrees`.`name`, `courses`.`name`;
+
 
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
