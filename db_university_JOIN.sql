d58670be4b1bc04d204191081be9e781ff606f7a
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-30 21:02:19 +0200

    JOIN 3 correction

--- db_university_JOIN.sql
+++ db_university_JOIN.sql
@@ -15,12 +15,14 @@ WHERE `departments`.`name`= 'Dipartimento di Neuroscienze'
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT `courses`.`name`
 FROM `courses`
-JOIN `course_teacher` ON `teachers`. `id` = `course_teacher`. `teacher_id` 
-JOIN `courses` ON `course_teacher`. `course_id` = `courses`. `id` 
-WHERE `teachers`.`id`= 44 OR  `teachers`.`name`= 'Fulvio Amato';
+JOIN `course_teacher` ON `courses`. `id` = `course_teacher`. `course_id`
+JOIN `teachers` ON `teachers`. `id` = `course_teacher`. `teacher_id` 
+WHERE `teachers`.`surname`= 'Amato' AND `teachers`.`name`= 'Fulvio';
 
 
-4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a 
+cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
